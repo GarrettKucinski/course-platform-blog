fb86e0d7d6d9d59168b1fa1656f8d9b2085b14ac
Garrett <garrett.kucinski@carvana.com>
2021-04-25 15:45:13 -0700

    remove dependencies that are unnecessary

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -1,4 +1,3 @@
-import cn from 'classnames'
 import Link from 'next/link'
 
 type Props = {
@@ -12,13 +11,10 @@ const CoverImage = ({ title, src, slug }: Props) => {
     <img
       src={src}
       alt={`Cover Image for ${title}`}
-      className={cn('shadow-small', {
-        'hover:shadow-medium transition-shadow duration-200': slug,
-      })}
     />
   )
   return (
-    <div className="sm:mx-0">
+    <div>
       {slug ? (
         <Link as={`/posts/${slug}`} href="/posts/[slug]">
           <a aria-label={title}>{image}</a>
